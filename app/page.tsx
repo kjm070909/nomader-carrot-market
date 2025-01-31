c632cc6c02957dee233380e76dfecd6a4c08a13b
kjm070909 <kjm070909@naver.com>
2025-01-31 20:15:40 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,36 @@
 "use client"
 
 import { useFormState } from "react-dom"
-import { handleForm } from "./actions"
+import { createAccount } from "./actions"
 
 export default function Home() {
-  const [state,action] = useFormState(handleForm,null)
+  const [state,action] = useFormState(createAccount,null)
+  function checkresult() {
+    console.log(state?.fieldErrors.email,state?.fieldErrors.password?.length,state?.fieldErrors.username?.length)
+    console.log(state)
+    if(state?.fieldErrors.email?.length === 0 && state?.fieldErrors.password?.length === 0 && state?.fieldErrors.username?.length === 0 )
+
+      return true
+    else{
+      return false
+    }
+  }
   return(
     <div className="m-auto mt-6 h-[100vh] w-[100vh] text-center justify-center ">
       <h1 className="text-[24px] font-semibold ">Login</h1>
       <form className="flex flex-col gap-3 p-[30px]" action={action}>
 
           <input type="email" name="email" placeholder="email" className=" rounded-xl p-1 border-2 text-black border-black  font-semibold"/>
+          
+          <div>{state?.fieldErrors.email?.map((error) => <span>{error}</span>)}</div> 
         <input type="name" name="name" placeholder="name" className=" rounded-xl p-1 border-2 border-black text-black  font-semibold"/>
+        <div>{state?.fieldErrors.username?.map((error) => <span>{error}</span>)}</div>
         <input type="password" name="password" placeholder="password" className=" rounded-xl p-1 border-2 border-black text-black  font-semibold"/>
-        <div>{state?.errors.map((error,key) => <span>{error =='good' ? "" : <span key={key} className="text-red-900">{error}</span>}</span>)}</div>
+        <div>{state?.fieldErrors.password?.map((error) => <li>{error}</li>)}</div>
 
         <button className="bg-gray-700 rounded-xl p-2">LOGIN</button>
-        <div>{state?.errors.map((error,key) => <span>{error =='good' ? <div className="bg-green-500 text-white rounded-xl p-2">Welcome Home!</div> : ''}</span>)}</div>
+        <div>{state === undefined ? <div className="p-2  bg-green-400 rounded-xl border-2 border-black">good job</div> : ''}</div>
+        {/* <div>{state?.errors.map((error: string,key: any) => <span>{error =='good' ? <div className="bg-green-500 text-white rounded-xl p-2">Welcome Home!</div> : ''}</span>)}</div> */}
       </form>
 
     </div>
