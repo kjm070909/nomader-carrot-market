0be82876bf34f6aef5748be1d54853d2c92384be
kjm070909 <kjm070909@naver.com>
2025-02-03 15:00:17 +0900

    test

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,37 @@
 "use server"
 import {z} from"zod"
 import validator from "validator"
+import { redirect } from "next/navigation"
 
+const phoneschema = z.string().trim().refine((phone) => validator.isMobilePhone(phone,"ko-KR"),"wrong phone number")
 
-
+const tokenSchema = z.coerce.number().min(100000).max(999999)
 
 export async function smsLogIn(prevState: any, formData: FormData) {
-  
+  const token = formData.get("token")
+  const phone = formData.get("phone")
+  if (!prevState.token) {
+    const result = phoneschema.safeParse(phone)
+    if (!result.success){
+        return {
+            token:false,
+            error:result.error.flatten(),
+        }
+    }else {
+        return{
+            token:true
+        }
+    }
+    
+  }else{
+    const result = tokenSchema.safeParse(token)
+    if(!result.success) {
+        return {
+            token:true,
+            error:result.error.flatten()
+        }
+    }else{
+        redirect("https://naver.com")
+    }
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@
 
 import { useFormState } from "react-dom";
 import { smsLogIn } from "./actions";
-import "@/lib/db"
+
+const startvalue = {
+  token:false,
+  error:undefined
+}
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogIn, null);
+  const [state, dispatch] = useFormState(smsLogIn, startvalue);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -13,16 +18,18 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <input name="phone" type="number" placeholder="Phone number" required />
-        <input
+        
+        {state.token ?        <input
+        className="text-black"
           name="token"
           type="number"
           placeholder="Verification code"
           required
           min={100000}
           max={999999}
-        />
-        <button value="Verify" />
+        /> : <input className="text-black" name="phone" type="number" placeholder="Phone number" required /> }
+
+        <button className="bg-orange-500 p-2 rounded-xl">{!state.token ? "phone number" : "code please"}</button>
       </form>
     </div>
   );
