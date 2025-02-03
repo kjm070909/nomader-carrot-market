de32e3d6204531b0a2532103c62bbe6ae2739601
kjm070909 <kjm070909@naver.com>
2025-02-03 13:45:36 +0900

    task

--- app/actions.ts
+++ app/actions.ts
@@ -1,37 +1,10 @@
-"use server";
-import {z} from "zod"
+"use server"
+import {z} from"zod"
+import validator from "validator"
 
-function checkemail(email:string){
-  return(
-    email.includes("@zod.com")
-    
-  )
-}
 
-const checkpassword = new RegExp(
-  '/[1-9]/'
-)
 
-const formschema = z.object({
-  email:z.string().refine(checkemail,'false email'),
-  username:z.string().min(5,'5글자 이상이어야합니다'),
-  password:z.string().regex(/[1-9]/, {
-    message: "비밀번호에는 1~9 사이의 숫자가 최소 하나 포함되어야 합니다.",
-  }).min(10,'비밀번호는 10글자 이상이어야 합니다')
-})
 
-export async function createAccount(prevState: any, formData: FormData) {
-  const data = {
-    email: formData.get('email'),
-    username:formData.get('name'),
-    password:formData.get('password'),
-
-  }
-  const result = formschema.safeParse(data)
-  if (!result.success) {
-    return result.error.flatten()
-  } else {
-    console.log('good')
-  }
-
+export async function smsLogIn(prevState: any, formData: FormData) {
+  
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,29 @@
-"use client"
+"use client";
 
-import { useFormState } from "react-dom"
-import { createAccount } from "./actions"
 
-export default function Home() {
-  const [state,action] = useFormState(createAccount,null)
-
-  return(
-    <div className="m-auto mt-6 h-[100vh] w-[100vh] text-center justify-center ">
-      <h1 className="text-[24px] font-semibold ">Login</h1>
-      <form className="flex flex-col gap-3 p-[30px]" action={action}>
-
-          <input type="email" name="email" placeholder="email" className=" rounded-xl p-1 border-2 text-black border-black  font-semibold"/>
-          
-          <div>{state?.fieldErrors.email?.map((error) => <span>{error}</span>)}</div> 
-        <input type="name" name="name" placeholder="name" className=" rounded-xl p-1 border-2 border-black text-black  font-semibold"/>
-        <div>{state?.fieldErrors.username?.map((error) => <span>{error}</span>)}</div>
-        <input type="password" name="password" placeholder="password" className=" rounded-xl p-1 border-2 border-black text-black  font-semibold"/>
-        <div>{state?.fieldErrors.password?.map((error) => <li>{error}</li>)}</div>
-
-        <button className="bg-gray-700 rounded-xl p-2">LOGIN</button>
-        <div>{state === undefined ? <div className="p-2  bg-green-400 rounded-xl border-2 border-black">good job</div> : ''}</div>
-        {/* <div>{state?.errors.map((error: string,key: any) => <span>{error =='good' ? <div className="bg-green-500 text-white rounded-xl p-2">Welcome Home!</div> : ''}</span>)}</div> */}
+import { useFormState } from "react-dom";
+import { smsLogIn } from "./actions";
+import "@/lib/db"
+export default function SMSLogin() {
+  const [state, dispatch] = useFormState(smsLogIn, null);
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">SMS Log in</h1>
+        <h2 className="text-xl">Verify your phone number.</h2>
+      </div>
+      <form action={dispatch} className="flex flex-col gap-3">
+        <input name="phone" type="number" placeholder="Phone number" required />
+        <input
+          name="token"
+          type="number"
+          placeholder="Verification code"
+          required
+          min={100000}
+          max={999999}
+        />
+        <button value="Verify" />
       </form>
-
     </div>
-  )
+  );
 }
